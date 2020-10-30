5c35d0aa6cfec449b7a4699635f9f47c5fb6531e
Itamar Cohen <itamar.c@taboola.com>
2020-10-30 16:37:29 +0200

    - fix user channel click

--- src/components/Sidebar/DirectMessage.js
+++ src/components/Sidebar/DirectMessage.js
@@ -3,14 +3,20 @@ import { HiOutlineHashtag } from 'react-icons/hi';
 import { useDispatch } from 'react-redux';
 import { ChannelName, HashIconChannel, StyledChannel } from './Channel.style';
 import { changePrivateChannel } from '../../features/channels/channelFlows';
+import { toggleSidebar } from '../../features/sidebar/sidebarSlice';
 
 const DirectMessage = ({ user, activeChannel }) => {
   const { displayName, id } = user;
   const dispatch = useDispatch();
 
+  const handleClick = () => {
+    dispatch(toggleSidebar());
+    dispatch(changePrivateChannel(user));
+  };
+
   return (
     <StyledChannel
-      onClick={() => dispatch(changePrivateChannel(user))}
+      onClick={handleClick}
       isActive={activeChannel.id === id}
     >
       <HashIconChannel>
